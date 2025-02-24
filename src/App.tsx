08e501155fa8226e0274a86c71f77772370ea2bc
Bruhat Kulkarni <138980593+bruhat15@users.noreply.github.com>
2025-02-24 22:12:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -195,7 +195,7 @@ function App() {
               </a>
 
               <button
-  onClick={() => navigate("/register")}
+  onClick={() => navigate("./register")}
   className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
 >
   Register Here
