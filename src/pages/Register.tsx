c88c1e529ee61301e23f7a933b9a7e981b408ef8
Bruhat Kulkarni <138980593+bruhat15@users.noreply.github.com>
2025-02-24 22:05:18 +0530

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -34,8 +34,9 @@ const Register = () => {
   };
 
   const handleNoCoupon = () => {
-    navigate('/dummy-form');
-  };
+  navigate("/dummy-form");
+};
+
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
