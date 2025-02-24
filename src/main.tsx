2049aa1af7eab643c9848e96fbbfb43ade90a98d
Bruhat Kulkarni <138980593+bruhat15@users.noreply.github.com>
2025-02-25 00:03:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import Register from "./pages/Register";
-import DummyForm from "./pages/DummyForm";
-import PaymentPage from "./pages/Payment";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dummy-form" element={<DummyForm />} />
-        <Route path="/payment" element={<PaymentPage />} />
-      </Routes>
-    </Router>
-  </StrictMode>
-);
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import Register from "./pages/Register";
-import DummyForm from "./pages/DummyForm";
-import PaymentPage from "./pages/Payment";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dummy-form" element={<DummyForm />} />
-        <Route path="/payment" element={<PaymentPage />} />
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
